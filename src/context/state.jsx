4ef968edb19789474d31df1ce05abdcc1367c895
Motasem-Sulaiman <motasemismael0@gmail.com>
2023-10-04 13:39:12 +0000

    add likes- delete likes

--- src/context/state.jsx
+++ src/context/state.jsx
@@ -7,18 +7,15 @@ export const StateContext = React.createContext();
 export default function State(props) {
   const [posts, setPosts] = useState([]);
   const [comments, setComments] = useState([]);
+  const [likes, setLikes] = useState([]);
   const [userData, setUserData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const authToken = cookie.load("auth");
   useEffect(() => {
-    if (authToken===null) {
-
-
+    if (authToken === null) {
       throw new Error("Authentication token not found.");
-    }else if(authToken!=null){
-      
-
+    } else if (authToken != null) {
       const headers = {
         Authorization: `Bearer ${authToken}`,
       };
@@ -26,52 +23,59 @@ export default function State(props) {
       axios
         .get("https://final-backend-nvf1.onrender.com/home/posts", { headers })
         .then((response) => {
-        
-
           setPosts(response.data);
         })
         .catch((error) => {
           setError(error);
         });
-
-
     }
-    if(authToken===null){
+    if (authToken === null) {
       throw new Error("Authentication token not found.");
-
-    }else if(authToken!=null){
-      
-
+    } else if (authToken != null) {
       const headers = {
         Authorization: `Bearer ${authToken}`,
       };
 
       axios
-        .get("https://final-backend-nvf1.onrender.com/home/comments", { headers })
+        .get("https://final-backend-nvf1.onrender.com/home/likes", { headers })
         .then((response) => {
-        
+          setLikes(response.data);
+        })
+        .catch((error) => {
+          setError(error);
+        });
+    }
+    if (authToken === null) {
+      throw new Error("Authentication token not found.");
+    } else if (authToken != null) {
+      const headers = {
+        Authorization: `Bearer ${authToken}`,
+      };
 
+      axios
+        .get("https://final-backend-nvf1.onrender.com/home/comments", {
+          headers,
+        })
+        .then((response) => {
           setComments(response.data);
         })
         .catch((error) => {
           setError(error);
         });
-
-
     }
-   
   }, []);
-
 
   const addPost = (newPost) => {
     setPosts([newPost, ...posts]);
+  };
+  const addLike = (newLike) => {
+    setLikes([newLike, ...likes]);
   };
 
   const addComment = (newComment) => {
     setComments([newComment, ...comments]);
   };
 
-  
   const editPost = (editedPost) => {
     setPosts((prevPosts) => {
       return prevPosts.map((post) => {
@@ -100,16 +104,24 @@ export default function State(props) {
     let newComments = state.comments.filter((item) => item.id != id);
     setComments(newComments);
   };
+  const deleteLike = (id) => {
+    let newLikes = state.likes.filter((item) => item.id != id);
+    setLikes(newLikes);
+  };
 
   const state = {
     posts: posts,
     comments: comments,
+    likes: likes,
     addPost: addPost,
     deletePost: deletePost,
     editPost: editPost,
-    addComment:addComment,
-    deleteComment:deleteComment,
-    editComments:editComments
+    addComment: addComment,
+    deleteComment: deleteComment,
+    editComments: editComments,
+
+    addLike: addLike,
+    deleteLike: deleteLike,
   };
 
   return (
